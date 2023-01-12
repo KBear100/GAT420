e8487e81775aa351c57bfa4fed79f9e293918bb5
KBear100 <kgamett@student.neumont.edu>
2023-01-11 19:52:02 -0700

    Autonomous Agent Steering and Spawner

--- Assets/Scripts/Agent/Perseption.cs
+++ Assets/Scripts/Agent/Perseption.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.XPath;
 using UnityEngine;
 
 public class Perseption : MonoBehaviour
 {
+    public string tagName = "";
     [Range(1, 40)] public float distance = 1.0f;
     [Range(0, 180)] public float maxAngle = 45.0f;
-    public string tagName = "";
 
     public GameObject[] GetGameObjects()
     {
@@ -19,18 +20,28 @@ public class Perseption : MonoBehaviour
             if (tagName == "" || collider.CompareTag(tagName))
             {
                 result.Add(collider.gameObject);
-            }
+                // calculate angle from transform forward vector to direction of game object 
+                Vector3 direction = (collider.transform.position - transform.position).normalized;
+                float angle = Vector3.Angle(transform.forward, direction);
 
-            // calculate angle from transform forward vector to direction of game object 
-            Vector3 direction = (collider.transform.position - transform.position).normalized;
-            float cos = Vector3.Dot(transform.forward, direction);
-            float angle = Mathf.Acos(cos) * Mathf.Rad2Deg;
-            if (angle <= maxAngle)
-            {
-                result.Add(collider.gameObject);
+                //float cos = Vector3.Dot(transform.forward, direction);
+                //float angle = Mathf.Acos(cos) * Mathf.Rad2Deg;
+                if (angle <= maxAngle)
+                {
+                    result.Add(collider.gameObject);
+                }
             }
         }
 
+        result.Sort(CompareDistance);
+
         return result.ToArray();
+    }
+
+    public int CompareDistance(GameObject a, GameObject b)
+    {
+        float squaredRangeA = (a.transform.position - transform.position).sqrMagnitude;
+        float squaredRangeB = (b.transform.position - transform.position).sqrMagnitude;
+        return squaredRangeA.CompareTo(squaredRangeB);
     }
 }
